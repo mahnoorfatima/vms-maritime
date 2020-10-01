517eea548839e17f0a438f6f7c6e544f9f2e0583
Mahnoor Fatima <mahnoorfatimabaloch94@hotmail.com>
2020-10-01 21:47:05 +0500

    Updates

---
+++ precache-manifest.0c0ef2cd4d0c863e48c6f97fa7bfc0bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b7914cbe2cf7ab94830a8c95f3eed2",
+    "url": "/vms-maritime/index.html"
+  },
+  {
+    "revision": "7453e63c3f5a0bca124d",
+    "url": "/vms-maritime/static/css/main.0547cee9.chunk.css"
+  },
+  {
+    "revision": "f06201892be46a2d6f85",
+    "url": "/vms-maritime/static/js/2.805ab63c.chunk.js"
+  },
+  {
+    "revision": "415632a8ecde893fcc4afa95fddbee9a",
+    "url": "/vms-maritime/static/js/2.805ab63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7453e63c3f5a0bca124d",
+    "url": "/vms-maritime/static/js/main.ac8c6b33.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/vms-maritime/static/js/main.ac8c6b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02b2473ea088eeb695e",
+    "url": "/vms-maritime/static/js/runtime-main.6b5e5270.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/vms-maritime/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/vms-maritime/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/vms-maritime/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/vms-maritime/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/vms-maritime/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/vms-maritime/static/media/react-logo.eb6be414.png"
+  }
+]);
